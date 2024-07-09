93ff3ea6f0c345fb9de5b3b6bff8fc89ecae3681
Pranthi <klpranathi18@gmail.com>
2024-07-08 20:06:34 -0500

    Implemented API Gateway

---
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,45 @@
+package com.in28minutes.microservices.apigateway;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+	// here we gona bulid beans for custom routing (hardcode)
+
+	@Bean
+	public RouteLocator gatewayRoutor(RouteLocatorBuilder builder) {
+		
+		
+		
+		return builder.routes()
+					.route(p -> p.path("/get")
+							.filters(f-> f.addRequestHeader("MyHeader", "MYURI")
+										  .addRequestParameter("Param", "MyValue"))
+							.uri("http://httpbin.org:80"))
+					.route(p -> p.path("/currency-exchange/**")
+					.uri("lb://currency-exchange/"))
+					
+					.route(p -> p.path("/currency-conversion/**")
+					.uri("lb://currency-conversion-service/"))
+					
+					.route(p -> p.path("/currency-conversion-feign/**")
+					.uri("lb://currency-conversion-service/"))
+					
+					.route(p -> p.path("/currency-conversion-new/**")
+							.filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)",
+									"/currency-conversion-feign/${segment}"))
+							.uri("lb://currency-conversion-service/"))
+					
+					.build();
+	}
+
+}
